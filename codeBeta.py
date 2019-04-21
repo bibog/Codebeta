fd5702e71f421bc7bfe85d02fda9c647fa359f03
bibog <bibocazz17@gmail.com>
2019-04-21 22:34:11 +0530

    python script commit

---
+++ codeBeta.py
@@ -0,0 +1,13 @@
+
+username = raw_input("Login:")
+
+user1 = "Danny"
+user2 = "Marga"
+
+
+if username == user1:
+    print "Access granted"
+elif username == user2:
+    print "Welcome to the system"
+else:
+    print "Access denied"
